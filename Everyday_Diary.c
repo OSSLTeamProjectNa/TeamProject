7b517c65dcd969a0f219b67ae09d345a42189e59
Siyun Na <0nasiyun1@handong.ac.kr>
2023-05-16 12:00:37 +0900

    CRUD 개발중입니다. (컴파일 완료, 테스트 전)

--- Everyday_Diary.c
+++ Everyday_Diary.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#include "diaryStruct.h"
+#include "diaryCRUD.h"
 
 int selectMenu(){
     int menu;
@@ -12,17 +8,50 @@ int selectMenu(){
     return menu;
 }
 
-int main(void){
+void listDiary(Diary *d[], int count){
+    printf("\nYYYY-MM-DD Title                             Locked\n");
+    printf("=====================================================\n");
+    for(int i =0; i <count ; i++){
+        if(d[i]->year[0] == -1) continue;
+        printf("%2d ", i+1);
+        readDiary(*d[i]);
+        }
+    printf("\n");
+}
+
+
+int selectNum(Diary *d[], int max){
+    listDiary(d,max);
+    int no;
+    printf("원하는 알기 번호는(취소 :0)? ");
+    scanf("%d", &no);
+
+    return no;
+}
+
+
+int main(){
     Diary *d[20];
     int menu, count, index;
     count = 0;
     index = 0;
 
+    menu = selectMenu();
+
     while (1){
         if (menu == 0) break;
-        if (menu == 1){printf("menu 1");}
-        else if (menu == 2){printf("menu 2");}
-        else if (menu == 3){printf("menu 3");}
+        if (menu == 1){listDiary(d, index);}
+        else if (menu == 2){
+            d[index] = (Diary *)malloc(sizeof(Diary));
+            addDiary(d[index]);
+            index++;
+            count ++;
+        }
+        else if (menu == 3){
+            int no = selectNum(d,index);
+            if (no == 0) printf("취소됨!\n");
+            else updateDiary(d[no-1]);
+        }
         else if (menu == 4){printf("menu 4");}
     }
         
