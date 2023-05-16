e8ee89459cfbc3336234443f7822a1d400ea5547
Siyun Na <0nasiyun1@handong.ac.kr>
2023-05-16 11:13:07 +0900

    struct Diary에 title data를 추가하였습니다.

--- diaryStruct.h
+++ diaryStruct.h
@@ -5,6 +5,7 @@ typedef struct diary{
     int month[2];
     int date[2];
     char names[100];
+    char title[15];
     char content[1000]; //공백포함
     int password [4];
     char weather[3]; //맑음, 바람, 비, 눈, 흐림 중에 설정
